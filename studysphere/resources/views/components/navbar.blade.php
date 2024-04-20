b1d89f54d4dcf939f534eef3c5792c63227afed6
Iverson Craig <Iversoncraigg@gmail.com>
2024-04-20 16:38:05 +0800

    Navbar Added Navbar Styling Added

---
+++ studysphere/resources/views/components/navbar.blade.php
@@ -0,0 +1,59 @@
+@section('navbar')
+<nav class="navbar">
+    <img src="{{ asset('sources/LogoBG.png') }}" alt="" id="logo" />
+    <h1>STUDYSPHERE</h1>
+    <div class="links">
+        <a href="/">Home</a>
+        <a href="/dashboard">Study Materials</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>                
+    </div>
+
+    <div class="userStatus">
+        @auth
+            <div class="user">
+                <span><b>{{ Auth::user()->firstname }}</b></span>
+            </div>
+            <div class="logout">
+                <!-- <a href="">Log Out</a> -->
+            </div>
+        @else
+            <div class="login">
+                <!-- <a href="">Login</a> -->
+                <a href="">Login</a>
+            </div>
+        @endauth
+    </div>
+
+    <div class="menubar">
+        <i class='bx bx-menu' onclick="collapseActive()"></i>
+    </div>
+
+    <div class="menuLinks">
+        <div class="menubar2">
+            <h2>StudySphere</h2>
+            <i class='bx bx-menu' onclick="collapse()"></i>
+        </div>
+
+        <div class="links2">
+            <a href="/">Home</a>
+            <a href="/dashboard">Study Materials</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a> 
+        </div>
+
+        <div class="userStatus2">
+            @auth
+                <div class="logout">
+                    <!-- <a href="">Log Out</a> -->
+                </div>
+            @else
+                <div class="login">
+                    <!-- <a href="">Login</a> -->
+                    <a href="">Login</a>
+                </div>
+            @endauth
+        </div>
+    </div>
+</nav>
+@endsection
